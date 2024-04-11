1a7b0ddc9b420015bccad605eec2c0eba047141f
NeshkaD <11585808+NeshkaD@users.noreply.github.com>
2024-04-11 12:30:22 -0400

    update serverjs (#25)

--- back-end/server.js
+++ back-end/server.js
@@ -1,25 +1,28 @@
 const express = require('express')
 const cors = require('cors');
+// const fileUpload = require('express-fileupload');
 const web_app = express();
 
 let corsOptions = {
     origin: "http://localhost:4200"
 };
 
-web_app.use(cors(corsOptions)); // enable CORS for http requests from Angular frontend.
-web_app.use(express.json()); // add JSON middleware from Express that parses requests.
-web_app.use(express.urlencoded({ extended: true })); // add URL encode/decode middleware from Express. 'extended' enables JSON-like experience for URL-encoded.
+web_app.use(cors(corsOptions)); // enable CORS for all http requests to the frontend (Angular) on port 8081
+web_app.use(express.json()); // add JSON middleware functionality from Express. Parses requests with JSON payloads.
+web_app.use(express.urlencoded({ extended: true })); // add URL encode/decode middleware functionality from Express. 'extended' is true to enable JSON-like experience with URL-encoded 
+// web_app.use(fileUpload); // add middleware for reading file from http POST form-data.
 
-const view_path = __dirname + '/src/views/'; //  not currently used to serve any frontend
+const view_path = __dirname + '/src/views/'; // directory containing the web pages (front end) to be served to client
 web_app.use(express.static(view_path));
 
-web_app.get('/', (req, res) => { // define root route.
+// define the root route
+web_app.get('/', (req, res) => {
   res.sendFile(path + "index.html");
 });
 
-require("./src/routes/routes.js")(web_app); // Add subroutes to web_app.
+require("./src/routes/routes.js")(web_app); // sub-routes are in separate folder. Apply them to the web app here by passing web_app as arg.
 
-const listen_port = process.env.PORT || 8080; // If port specified at runtime then use it. Else default port is 8080.
-web_app.listen(listen_port, () => console.log(`Server connection established on port ${listen_port}`)); // Listen for requests.
+const listen_port = process.env.PORT || 8080; // If port is specified by runtime environment, then use that. Otherwise default to port 8080.
+web_app.listen(listen_port, () => console.log(`Server connection established on port ${listen_port}`));
 
 module.exports = web_app;
